2eeffa49c4549e155ae4a89b3575e78351096d4f
sirGOLDU <73430077+sirGOLDU@users.noreply.github.com>
2023-02-25 16:18:16 +0530

    Create clientB.py

---
+++ clientB.py
@@ -0,0 +1,7 @@
+import socket
+c = socket.socket()
+c.connect(("localhost", 9999))
+hello = input("send message >> ")
+c.send(bytes(hello,'utf-8'))
+p = c.recv(1024).decode()
+print("recieved >> ", p)
