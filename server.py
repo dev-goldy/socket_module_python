b939864652a55154ec6defd5dde2069a98931c9e
sirGOLDU <73430077+sirGOLDU@users.noreply.github.com>
2023-02-25 16:15:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 #check for free port numbers if your using in termux 
-
 import socket 
-s = socket.socket()                #creating server socket 
-s.bind(("localhost", 9999))        #you change port number use free one  
-s.listen(4)                        #will listen to 4 connections
-while True:
-  c, addr = s.accept()
-  print("connected with client >> ", addr)
-  c.send(bytes("messages from server >> hello friend ", "utf-8"))
-  c.close()
+s = socket.socket() 
+IP = "localhost" # input("enter ip addr > ") 
+PORT = 9999 # int(input("enter port num > ")) 
+s.bind((IP, PORT)) 
+s.listen(3) 
+print("listening...:") 
+
+while True: 
+    a, addr = s.accept()
+    c, addr = s.accept() 
+    message = c.recv(1024).decode() 
+    while True:
+        if a.recv(1024).decode() == "hello": 
+            c.send(bytes("hello", 'utf-8'))
+        elif c.recv(1024).decode() == "hello":
+            a.send(bytes("bie bie :) ", 'utf-8'))
+        # else:
+        #     c.send(bytes("bye",'utf-8'))
+        print("connected with client >> ", addr, "message >> ", message) 
+        c.close() 
