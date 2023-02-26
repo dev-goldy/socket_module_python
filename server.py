107d8569673b343d77db0bfb2444f6432b1e7259
sirGOLDU <73430077+sirGOLDU@users.noreply.github.com>
2023-02-26 15:06:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,40 @@
 #check for free port numbers if your using in termux 
-import socket 
-s = socket.socket() 
-IP = "localhost" # input("enter ip addr > ") 
-PORT = 9999 # int(input("enter port num > ")) 
-s.bind((IP, PORT)) 
-s.listen(3) 
-print("listening...:") 
+import socket
 
-while True: 
-    a, addr = s.accept()
-    c, addr = s.accept() 
-    message = c.recv(1024).decode() 
+s = socket.socket()
+IP = "localhost"
+PORT = 9999
+s.bind((IP, PORT))
+s.listen(2)
+print("Listening...")
+
+while True:
+    a, addr1 = s.accept()
+    print("Connected with client 1:", addr1)
+    
+    c, addr2 = s.accept()
+    print("Connected with client 2:", addr2)
+
     while True:
-        if a.recv(1024).decode() == "hello": 
+        message = a.recv(1024).decode()
+        if message == "hello":
+            print("Client 1 says hello")
             c.send(bytes("hello", 'utf-8'))
-        elif c.recv(1024).decode() == "hello":
-            a.send(bytes("bie bie :) ", 'utf-8'))
-        # else:
-        #     c.send(bytes("bye",'utf-8'))
-        print("connected with client >> ", addr, "message >> ", message) 
-        c.close() 
+        else:
+            print("Client 1 disconnected")
+            c.send(bytes("bye", 'utf-8'))
+            break
+
+        message = c.recv(1024).decode()
+        if message == "hello":
+            print("Client 2 says hello")
+            a.send(bytes("bye", 'utf-8'))
+            break
+        else:
+            print("Client 2 disconnected")
+            a.send(bytes("bye", 'utf-8'))
+            break
+
+    a.close()
+    c.close()
+
